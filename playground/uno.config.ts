a6fff29b46003fc9048523c757c49d17065a55ab
lwj <jg15575446613@163.com>
2025-03-18 08:45:42 +0800

    feat: 新特性添加

--- playground/uno.config.ts
+++ playground/uno.config.ts
@@ -2,11 +2,14 @@ import { defineConfig, presetUno, presetAttributify } from 'unocss'
 import { presetDynamic } from '../src'
 import { fileURLToPath } from 'url'
 import path from 'path'
-import { alias } from './vite.config'
+// import { alias } from './vite.config'
 
 // Just for Vscode Extension
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const alias = {
+  '@': path.resolve(__dirname, 'src')
+}
 export default defineConfig({
   presets: [
     presetUno(),
