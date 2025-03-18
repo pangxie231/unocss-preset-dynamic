aa3ebb1e6b8473dcce9065cb5b62ce2eb5cf1986
lwj <jg15575446613@163.com>
2025-03-18 11:11:30 +0800

    feat:图片读取问题修复

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -2,7 +2,6 @@
   <div>
     <div class="flex" bg-dynamic="@/assets/imgs/dog.jpg"></div>
     <img size-dynamic="@/assets/imgs/dog.jpg" src="@/assets/imgs/dog.jpg" alt="">
-    <div bg-dynamic="@/assets/images/common/part1/main-bg.png"></div>
   </div>
 
 
